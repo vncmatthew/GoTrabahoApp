d7ca0cfe9b2980ce8a4d826e68a9553577a99545
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-10 23:16:41 +0800

    Created UI for login and register

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/FreelancerRegisterDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreelancerRegisterDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_freelancer_register_details)
+    }
+}
