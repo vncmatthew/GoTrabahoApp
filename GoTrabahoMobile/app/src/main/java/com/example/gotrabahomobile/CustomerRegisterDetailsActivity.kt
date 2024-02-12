c39bc9255200777fc777b99c1f98cc4bd6eee28a
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-12 18:32:06 +0800

    renamed textboxes in customer register

--- GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/CustomerRegisterDetailsActivity.kt
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/CustomerRegisterDetailsActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class CustomerRegisterDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_details)
+        setContentView(R.layout.activity_customer_register_details)
     }
 }
