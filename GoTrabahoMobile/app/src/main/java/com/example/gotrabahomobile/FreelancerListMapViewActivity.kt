9ca9887ae8bc07c352b3634ea7b7d6ddfab2a7e6
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-18 14:47:22 +0800

    changed birthdate format, added new appbar, created map view

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/FreelancerListMapViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreelancerListMapViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_freelancer_list_map_view)
+    }
+}
--- GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/FreelancerRegisterDetailsActivity.kt
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/FreelancerRegisterDetailsActivity.kt
@@ -75,7 +75,7 @@ class FreelancerRegisterDetailsActivity : AppCompatActivity() {
         birthdateText.addTextChangedListener(object : TextWatcher {
 
             private var current = ""
-            private val ddmmyyyy = "DDMMYYYY"
+            private val yyyymmdd = "YYYYMMDD"
             private val cal = Calendar.getInstance()
 
              override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -94,13 +94,13 @@ class FreelancerRegisterDetailsActivity : AppCompatActivity() {
                     if (clean == cleanC) sel--
 
                     if (clean.length < 8) {
-                        clean = clean + ddmmyyyy.substring(clean.length)
+                        clean = clean + yyyymmdd.substring(clean.length)
                     } else {
                         //This part makes sure that when we finish entering numbers
                         //the date is correct, fixing it otherwise
-                        var day = Integer.parseInt(clean.substring(0, 2))
-                        var mon = Integer.parseInt(clean.substring(2, 4))
-                        var year = Integer.parseInt(clean.substring(4, 8))
+                        var day = Integer.parseInt(clean.substring(0, 4))
+                        var mon = Integer.parseInt(clean.substring(4, 6))
+                        var year = Integer.parseInt(clean.substring(6, 8))
 
                         mon = if (mon < 1) 1 else if (mon > 12) 12 else mon
                         cal.set(Calendar.MONTH, mon - 1)
@@ -111,12 +111,12 @@ class FreelancerRegisterDetailsActivity : AppCompatActivity() {
                         //would be automatically corrected to 28/02/2012
 
                         day = if (day > cal.getActualMaximum(Calendar.DATE)) cal.getActualMaximum(Calendar.DATE) else day
-                        clean = String.format("%02d%02d%02d", day, mon, year)
+                        clean = String.format("%02d%02d%02d", year, mon, day)
                     }
 
-                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
-                        clean.substring(2, 4),
-                        clean.substring(4, 8))
+                    clean = String.format("%s/%s/%s", clean.substring(0, 4),
+                        clean.substring(4, 6),
+                        clean.substring(6, 8))
 
                     sel = if (sel < 0) 0 else sel
                     current = clean
