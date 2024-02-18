1624b291b4f089d1f028ac337aa45986392d3b99
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-18 15:48:59 +0800

    created freelancerbookingspage, added in strings.xml

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/FreelancerBookingsPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreelancerBookingsPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_freelancer_bookings_page)
+    }
+}
