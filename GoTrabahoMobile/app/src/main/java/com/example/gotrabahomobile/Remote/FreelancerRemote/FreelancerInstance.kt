7bf28c4ebc5e63d732e8ea0416095a816a70e9ce
georgeencarnacion <georgeangelo.encarnacion@benilde.edu.ph>
2024-02-19 21:42:05 +0800

    Revert "Made Changes"
    This reverts commit 864bef582eda050c90a5aa02d8c8856e7080e3ee.

--- GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/Remote/FreelancerRemote/FreelancerInstance.kt
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/Remote/FreelancerRemote/FreelancerInstance.kt
@@ -1,8 +1,6 @@
 package com.example.gotrabahomobile.Remote.FreelancerRemote
 
 import com.example.gotrabahomobile.Remote.ArchiveRecordRemote.ArchiveRecordInterface
-import com.example.gotrabahomobile.Remote.UserRemote.UserInstance
-import com.example.gotrabahomobile.Remote.UserRemote.UserInterface
 import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
--- GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/Remote/FreelancerRemote/FreelancerInterface.kt
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/Remote/FreelancerRemote/FreelancerInterface.kt
@@ -1,23 +1,16 @@
 package com.example.gotrabahomobile.Remote.FreelancerRemote
 
-import com.example.gotrabahomobile.DTO.FreelancerDTO
 import com.example.gotrabahomobile.Model.Booking
 import com.example.gotrabahomobile.Model.Freelancer
 import com.example.gotrabahomobile.Model.FreelancerTesdaCertificate
 import com.example.gotrabahomobile.Model.proofOfExperience
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.Multipart
-import retrofit2.http.Part
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface FreelancerInterface {
     @GET("api/Freelancer")
@@ -47,30 +40,25 @@ interface FreelancerInterface {
     @GET("api/Freelancer/proof/{freelancerId}")
     fun getProofs(@Path("proofId") proofId: Int): Call<proofOfExperience>
 
-
-    @Multipart
     @POST("api/Freelancer/CreateFreelancer")
-    fun createFreelancer(
-        @Part("userId") userId: Int,
-        @Part imageFile: MultipartBody.Part,
-        @Part("freelancerDTO") freelancerDTO: RequestBody
-    ): Call<Freelancer>
+    fun insertFreelancer(@Body request: Freelancer): Call<Freelancer>
 
+    @POST("api/Freelancer/CreateTesdaCertificate")
+    fun insertCertificate(@Body request: FreelancerTesdaCertificate): Call<FreelancerTesdaCertificate>
 
-    @POST("CreateProof")
+    @POST("api/Freelancer/CreateProof")
     fun insertProof(@Body request: proofOfExperience): Call<proofOfExperience>
 
     @PUT("api/Freelancer/{freelancerId}")
     fun updateFreelancer(@Path("freelancerId") resourceId: String, @Body updatedResource: Freelancer): Call<Freelancer>
-    @PUT("UpdateProof/{proofId")
+    @PUT("api/Freelancer/{proofId")
     fun updateProof(@Path("proofId") resourceId: String, @Body updatedResource: proofOfExperience): Call<proofOfExperience>
-    @PUT("UpdateCertificate/{tesdaId}")
+    @PUT("api/Freelancer/{tesdaId}")
     fun updateTesda(@Path("tesdaId") resourceId: String, @Body updatedResource: FreelancerTesdaCertificate): Call<FreelancerTesdaCertificate>
     @DELETE("api/Freelancer/{freelancerId}")
     fun deleteFreelancer(@Path("freelancerId") resourceId: String): Call<Freelancer>
-    @DELETE("DeleteCertificate/{tesdaId}")
+    @DELETE("api/Freelancer/{tesdaId}")
     fun deleteTesda(@Path("tesdaId") resourceId: String): Call<FreelancerTesdaCertificate>
-    @DELETE("DeleteProof/{proofId}")
+    @DELETE("api/Freelancer/{proofId}")
     fun deleteProof(@Path("proofId") resourceId: String): Call<proofOfExperience>
-
 }
