bb06efe4085d1fefee65060d127b10c6e15adb27
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-21 00:07:37 +0800

    Created Await Application Confirmation Page

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/ApplicationConfirmationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplicationConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application_confirmation)
+    }
+}
