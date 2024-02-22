da91f370a594ac63af09898af5bc34855ebf6fe9
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-22 22:28:42 +0800

    created freelancer navbar

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/FreelancerMainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class FreelancerMainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_freelancer_main)
+
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}
