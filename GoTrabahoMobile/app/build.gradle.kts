a47e745f3385ec1799cf89d8f033ecddb9a79be9
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-23 14:02:41 +0800

    osmdroid library

--- GoTrabahoMobile/app/build.gradle.kts
+++ GoTrabahoMobile/app/build.gradle.kts
@@ -62,5 +62,7 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
 
+    implementation ("org.osmdroid:osmdroid-android:6.1.18")
+
 
 }
