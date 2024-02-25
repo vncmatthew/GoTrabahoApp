4327ea3f526408683da08d633759fa358112bd05
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-25 21:10:21 +0800

    created dialogbox for set price

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/RewardsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RewardsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rewards)
+    }
+}
