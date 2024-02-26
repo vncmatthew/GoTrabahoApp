c71bc3307b3058b695d90a235220ab60c339223c
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-02-26 22:33:53 +0800

    created layout for chat and freelancermessagesactivity ui

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/FreelancerMessagesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreelancerMessagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_freelancer_messages)
+    }
+}
