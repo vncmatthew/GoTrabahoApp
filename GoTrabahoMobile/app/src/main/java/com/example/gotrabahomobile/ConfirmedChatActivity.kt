aef233320ec74b2b3cabd8cc6da40a461d5a72ea
vncmatthew <128977820+vncmatthew@users.noreply.github.com>
2024-03-15 19:48:28 +0800

    confirmedchatactivity

---
+++ GoTrabahoMobile/app/src/main/java/com/example/gotrabahomobile/ConfirmedChatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gotrabahomobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmedChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmed_chat)
+    }
+}
